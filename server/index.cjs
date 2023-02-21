7302148e4bb77d2fbdb858893fb11ce8a524bd90
Lukas Jørgensen <lukas.dinven@gmail.com>
2023-02-21 10:27:33 -0800

    .

--- server/index.cjs
+++ server/index.cjs
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Server } = require('colyseus');
 const { monitor } = require('@colyseus/monitor');
 const { WebSocketTransport } = require('@colyseus/ws-transport');
-const https = require('https');
+const https = require('http');
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
@@ -11,25 +11,35 @@ const { TicTacToe } = require('./rooms/TicTacToeRoom.cjs');
 
 const port = Number(env.PORT || 2567);
 
-const options = {
-	key: fs.readFileSync(env.KEY),
-	cert: fs.readFileSync(env.CERT),
-	ca: [
-		fs.readFileSync(env.CAROOT),
-		fs.readFileSync(env.CABUNDLE)
-	]
-}
+// const options = {
+// 	key: fs.readFileSync(env.KEY),
+// 	cert: fs.readFileSync(env.CERT),
+// 	ca: [
+// 		fs.readFileSync(env.CAROOT),
+// 		fs.readFileSync(env.CABUNDLE)
+// 	]
+// }
 
 const app = express();
-app.use(cors({origin: '*'}));
+app.use(express.json(),cors({origin: '*'}));
 const gameServer = new Server({
 	transport: new WebSocketTransport({
-		server: https.createServer( options, app)
+		server: https.createServer(app)
 	})
 });
 
 // register your room handlers
 gameServer.define('tictactoe', TicTacToe);
+
+app.get('/data', (req, res) => {
+	// send back all the files in base directory
+	res.json(fs.readdirSync('./'));
+});
+
+app.get('/data/:folder', (req, res) => {
+	// send back the folder in requested directory
+	res.json(fs.readdirSync(`./${req.params.file}`));
+});
 
 app.use("/colyseus", monitor());
 
