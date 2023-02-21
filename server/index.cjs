3524e24f4f3df930d8e13e5fea5eb54beebbe80d
Lukas Jørgensen <lukas.dinven@gmail.com>
2023-02-20 21:21:52 -0800

    added monitor

--- server/index.cjs
+++ server/index.cjs
@@ -1,6 +1,7 @@
 const { env } = require('process');
 require('dotenv').config();
 const { Server } = require('colyseus');
+const { monitor } = require('@colyseus/monitor');
 const { WebSocketTransport } = require('@colyseus/ws-transport');
 const http = require('http');
 const express = require('express');
@@ -19,6 +20,8 @@ const gameServer = new Server({
 
 // register your room handlers
 gameServer.define('tictactoe', TicTacToe);
+
+app.use("/colyseus", monitor());
 
 gameServer.listen(port);
 console.log(`Listening on ws://localhost:${port}`);
