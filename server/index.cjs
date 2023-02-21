5292c83002c87c386225eb74c053a6b4c7546bf5
Lukas Jørgensen <lukas.dinven@gmail.com>
2023-02-21 13:52:14 -0800

    change to run on 1 instance

--- server/index.cjs
+++ server/index.cjs
@@ -3,65 +3,56 @@ require('dotenv').config();
 const { Server } = require('colyseus');
 const { monitor } = require('@colyseus/monitor');
 const { WebSocketTransport } = require('@colyseus/ws-transport');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
+// const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const { TicTacToe } = require('./rooms/TicTacToeRoom.cjs');
 
-const port = Number(env.PORT || 2567);
-
-const options = {
-	key: fs.readFileSync(env.KEY),
-	cert: fs.readFileSync(env.CERT),
-	ca: [
-		fs.readFileSync(env.CAROOT),
-		fs.readFileSync(env.CABUNDLE)
-	]
-}
+const port = Number(env.SERVERPORT || 2567);
 
 const app = express();
 app.use(express.json(),cors({origin: '*'}));
 const gameServer = new Server({
 	transport: new WebSocketTransport({
-		server: https.createServer(options, app)
+		server: http.createServer(app)
 	})
 });
 
 // register your room handlers
 gameServer.define('tictactoe', TicTacToe);
 
-function getFileOrDirectory(file, path) {
-	if (fs.existsSync(path + file) && fs.lstatSync(path + file).isDirectory()) {
-		const newPath = path + file + '/';
-		return {
-			name: file,
-			type: 'folder',
-			path: newPath,
-			children: fs.readdirSync(`${path}/${file}`).map((file) => {
-				return getFileOrDirectory(file, newPath);
-			})
-		};
-	} else {
-		return {
-			name: file,
-			type: 'file'
-		};
-	}
-}
+// function getFileOrDirectory(file, path) {
+// 	if (fs.existsSync(path + file) && fs.lstatSync(path + file).isDirectory()) {
+// 		const newPath = path + file + '/';
+// 		return {
+// 			name: file,
+// 			type: 'folder',
+// 			path: newPath,
+// 			children: fs.readdirSync(`${path}/${file}`).map((file) => {
+// 				return getFileOrDirectory(file, newPath);
+// 			})
+// 		};
+// 	} else {
+// 		return {
+// 			name: file,
+// 			type: 'file'
+// 		};
+// 	}
+// }
 
-app.get('/data', (req, res) => {
-	// send back all the files in base directory
-	// const data = fs.readdirSync('./').forEach((file) => {
-	// 	if (file === 'node_modules') return;
-	// 	getFileOrDirectory(file, './');
-	// });
-	const data = fs.readdirSync('./').map((file) => {
-		if (file === 'node_modules') return;
-		return getFileOrDirectory(file, './');
-	});
-	res.json(data);
-});
+// app.get('/data', (req, res) => {
+// 	// send back all the files in base directory
+// 	// const data = fs.readdirSync('./').forEach((file) => {
+// 	// 	if (file === 'node_modules') return;
+// 	// 	getFileOrDirectory(file, './');
+// 	// });
+// 	const data = fs.readdirSync('./').map((file) => {
+// 		if (file === 'node_modules') return;
+// 		return getFileOrDirectory(file, './');
+// 	});
+// 	res.json(data);
+// });
 
 app.use("/colyseus", monitor());
 
